3942466d70554d17da4935a5059cb6bae3641b7d
intikatimi <84741553+intikatimi@users.noreply.github.com>
2021-07-21 11:13:39 +0300

    Sondgs.py

--- handlers/songs.py
+++ handlers/songs.py
@@ -1,24 +1,10 @@
-from __future__ import unicode_literals
-
-import asyncio
-import math
 import os
-import time
-from random import randint
-from urllib.parse import urlparse
-
-import aiofiles
-import aiohttp
 import requests
-import wget
+import aiohttp
 import youtube_dl
-from pyrogram import Client, filters
-from pyrogram.errors import FloodWait, MessageNotModified
-from pyrogram.types import Message
-from youtube_search import YoutubeSearch
-from youtubesearchpython import SearchVideos
 
-from samantha.modules.play import arq
+from pyrogram import filters, Client
+from youtube_search import YoutubeSearch
 
 
 @Client.on_message(filters.command("bul") & ~filters.channel)
